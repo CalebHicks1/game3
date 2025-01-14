d8c46e0b09594d80beaa7ddf324d1e990cb1859e
Caleb Hicks <calebhicks1@gmail.com>
2025-01-14 12:30:41 -0500

    added fragment shader

--- main.go
+++ main.go
@@ -148,8 +148,35 @@ func run() {
 		runSpeed:  400.0,
 	}
 
+	// SHADER ///////////////////////////////////////////////////////////////////////////////////////////////
+	var fragmentShader = `
+		    #version 330 core
+			// The first line in glsl source code must always start with a version directive as seen above.
+
+			// vTexCoords are the texture coordinates, provided by Pixel
+			in vec2  vTexCoords;
+
+			// fragColor is what the final result is and will be rendered to your screen.
+			out vec4 fragColor;
+
+			// uTexBounds is the texture's boundries, provided by Pixel.
+			uniform vec4 uTexBounds;
+
+			// uTexture is the actualy texture we are sampling from, also provided by Pixel.
+			uniform sampler2D uTexture;
+
+			void main() {
+				// t is set to the screen coordinate for the current fragment.
+				vec2 t = (vTexCoords - uTexBounds.xy) / uTexBounds.zw;
+				// And finally, we're telling the GPU that this fragment should be the color as sampled from our texture.
+				// fragColor = texture(uTexture, t);
+				fragColor = vec4(uTexBounds.w, 0, 0, 1);
+			}
+		`
+
 	// GAME LOOP /////////////////////////////////////////////////////////////////////////////////////////////
 	for !win.Closed() {
+		win.Canvas().SetFragmentShader(fragmentShader)
 		dt := time.Since(last).Seconds()
 		last = time.Now()
 
